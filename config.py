3576ad331cb8ed447c7fd1a82494829fd3cf1f1b
er-gv <erezg3@gmail.com>
2025-03-23 10:33:10 +0200

    push erez local to repo

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int = 5432
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str | None = None
-    POSTGRES_PASSWORD_FILE: str | None = None
+    POSTGRES_PASSWORD_FILE: str | None = None #"db/password.txt"
     POSTGRES_DB: str
 
     @model_validator(mode="before")
@@ -26,6 +26,7 @@ class Settings(BaseSettings):
         if isinstance(data, dict):
             if data.get("POSTGRES_PASSWORD_FILE") is None and data.get("POSTGRES_PASSWORD") is None:
                 raise ValueError("At least one of POSTGRES_PASSWORD_FILE and POSTGRES_PASSWORD must be set.")
+        
         return data
 
     @validator('POSTGRES_PASSWORD_FILE', pre=True, always=True)
